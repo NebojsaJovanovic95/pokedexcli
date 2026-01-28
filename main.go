6385dfb1fa4c991ef046d365790c63cc58a7df06
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-28 10:02:01 -0500

    quicksave

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+	"os"
+	"bufio"
 )
 
 func cleanInput(text string) []string {
@@ -10,6 +12,22 @@ func cleanInput(text string) []string {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	var line string;
+	for ;; {
+		fmt.Print("Pokedex > ")
+		more := scanner.Scan();
+		err := scanner.Err();
+		if err != nil {
+			fmt.Println("Error: %w", err)
+		}
+		if more {
+			line = scanner.Text();
+			commands := cleanInput(line)
+			fmt.Println("Your command was:", commands[0])
+		} else {
+			break
+		}
+	}
 }
 
